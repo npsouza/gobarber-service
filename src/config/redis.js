f2a34fc934bd60ce5d3afa29db14c4da0bd346d2
Nathan <nathanpereirasouza@gmail.com>
2019-12-22 15:01:01 -0300

    alterando configuração do redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,7 +1,7 @@
 const config = {};
 
 if (process.env.REDIS_URL) {
-  config.path = process.env.REDIS_URL;
+  config.url = process.env.REDIS_URL;
 } else {
   config.host = process.env.REDIS_HOST;
   config.port = process.env.REDIS_PORT;
