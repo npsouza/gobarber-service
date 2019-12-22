2d129dd178e7a09b2d89c42dc702a4bf6da03a2b
Nathan <nathanpereirasouza@gmail.com>
2019-12-22 13:17:39 -0300

    alterando variaveis de config redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,4 +1,10 @@
-export default {
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-};
+const config = {};
+
+if (process.env.REDIS_URL) {
+  config.path = process.env.REDIS_URL;
+} else {
+  config.host = process.env.REDIS_HOST;
+  config.port = process.env.REDIS_PORT;
+}
+
+export default config;
