a074a3488f3cc2b5264c370118f94a82bc7448dc
Nathan <nathanpereirasouza@gmail.com>
2019-12-22 14:49:29 -0300

    alterando configuração do sequelize

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,22 @@
 require('dotenv/config');
 
-module.exports = {
+const databaseConfig = {
   dialect: 'postgres',
-  host: process.env.DB_HOST,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
   define: {
     timestamps: true,
     underscored: true,
     underscoredAll: true,
   },
 };
+
+if (process.env.DATABASE_URL) {
+  databaseConfig.uri = process.env.DATABASE_URL;
+  databaseConfig.native = true;
+} else {
+  databaseConfig.host = process.env.DB_HOST;
+  databaseConfig.password = process.env.DB_PASS;
+  databaseConfig.database = process.env.DB_NAME;
+  databaseConfig.username = process.env.DB_USER;
+}
+
+module.exports = databaseConfig;
--- src/database/index.js
+++ src/database/index.js
@@ -16,7 +16,11 @@ class Database {
   }
 
   init() {
-    this.connection = new Sequelize(databaseConfig);
+    if (databaseConfig.uri) {
+      this.connection = new Sequelize(databaseConfig.uri, databaseConfig);
+    } else {
+      this.connection = new Sequelize(databaseConfig);
+    }
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
